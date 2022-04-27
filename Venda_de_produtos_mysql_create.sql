2568b075814b3cc46cad2b2b0fee9bbd0051f09c
joaopedro214 <joaopedro.gonzatti@hotmail.com>
2022-04-27 20:35:50 -0300

    arq venda e prod

---
+++ Venda_de_produtos_mysql_create.sql
@@ -0,0 +1,92 @@
+CREATE TABLE `Produtos` (
+	`id` INT NOT NULL,
+	`nome` varchar(255) NOT NULL AUTO_INCREMENT,
+	`estoque` varchar(255) NOT NULL,
+	`marca` varchar(255) NOT NULL,
+	`categoria_id` INT NOT NULL,
+	`forn_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Clientes` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nome` varchar(255) NOT NULL AUTO_INCREMENT,
+	`cpf` varchar(255) NOT NULL AUTO_INCREMENT,
+	`uf` varchar(255) NOT NULL AUTO_INCREMENT,
+	`cidade` varchar(255) NOT NULL AUTO_INCREMENT,
+	`bairro` varchar(255) NOT NULL AUTO_INCREMENT,
+	`cep` varchar(255) NOT NULL AUTO_INCREMENT,
+	`rua` varchar(255) NOT NULL AUTO_INCREMENT,
+	`endereco` varchar(255) NOT NULL AUTO_INCREMENT,
+	`telefone` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Pedidos` (
+	`id` INT NOT NULL,
+	`valor` FLOAT NOT NULL,
+	`data` DATE NOT NULL,
+	`forma_pagamento` varchar(255) NOT NULL,
+	`cliente_id` INT NOT NULL,
+	`vendedor_id` INT NOT NULL,
+	`status_ped_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Categorias` (
+	`id` INT NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Fornecedores` (
+	`id` INT NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`cnpj` varchar(255) NOT NULL,
+	`telefone` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Vendedor` (
+	`id` INT NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`telefone` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Produtos_pedido` (
+	`id` INT NOT NULL,
+	`pedido_id` INT NOT NULL,
+	`produtos_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Status_ped` (
+	`id` INT NOT NULL,
+	`descricao` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `Produtos` ADD CONSTRAINT `Produtos_fk0` FOREIGN KEY (`categoria_id`) REFERENCES `Categorias`(`id`);
+
+ALTER TABLE `Produtos` ADD CONSTRAINT `Produtos_fk1` FOREIGN KEY (`forn_id`) REFERENCES `Fornecedores`(`id`);
+
+ALTER TABLE `Pedidos` ADD CONSTRAINT `Pedidos_fk0` FOREIGN KEY (`cliente_id`) REFERENCES `Clientes`(`id`);
+
+ALTER TABLE `Pedidos` ADD CONSTRAINT `Pedidos_fk1` FOREIGN KEY (`vendedor_id`) REFERENCES `Vendedor`(`id`);
+
+ALTER TABLE `Pedidos` ADD CONSTRAINT `Pedidos_fk2` FOREIGN KEY (`status_ped_id`) REFERENCES `Status_ped`(`id`);
+
+ALTER TABLE `Produtos_pedido` ADD CONSTRAINT `Produtos_pedido_fk0` FOREIGN KEY (`pedido_id`) REFERENCES `Pedidos`(`id`);
+
+ALTER TABLE `Produtos_pedido` ADD CONSTRAINT `Produtos_pedido_fk1` FOREIGN KEY (`produtos_id`) REFERENCES `Produtos`(`id`);
+
+
+
+
+
+
+
+
+
