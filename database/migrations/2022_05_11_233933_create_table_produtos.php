f9f509c7f15fc624dde2a943c139b9ef2eb9d645
Patrick <patrickonesradaelli@gmail.com>
2022-05-11 20:55:55 -0300

    comentario23

---
+++ database/migrations/2022_05_11_233933_create_table_produtos.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProdutos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 255);
+            $table->string('estoque', 255);
+            $table->string('marca', 255);
+            $table->foreignId('categoria_id')->constrained('categorias');
+            $table->foreignId('forn_id')->constrained('fornecedores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_produtos');
+    }
+}
